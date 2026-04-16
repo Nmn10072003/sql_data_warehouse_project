806e6144202c8a8fc98df0ae55cd5e513d60434e
Nmn10072003 <131520467+Nmn10072003@users.noreply.github.com>
2026-04-16 12:06:13 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,15 +1,20 @@
 USE master;
 
+/* If the database DataWarehouse is already present, DROP it */
+
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWarehouse;
 END;
 
+-- Create Databse DataWarehouse
+
 CREATE DATABASE DataWarehouse;
 
 USE DataWarehouse;
 
+-- Create Schemas
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
 CREATE SCHEMA gold;
